95cf0f78d309f888dfe433534a6e2f74e6eba19c
hhhisedward <102205131+HuaEdward@users.noreply.github.com>
2024-04-16 17:26:15 -0400

    Update Minting.sol

--- Contracts/Minting.sol
+++ Contracts/Minting.sol
@@ -10,16 +10,26 @@ contract PawNFT is ERC721, Ownable {
     mapping(address => bool) private _hasMinted;
     uint256 private _scoreThreshold = 6500;
 
+    mapping(uint256 => string) private _tokenSpecies;
+
     constructor(
         address initialOwner
     ) ERC721("PawNFT", "Paw") Ownable(initialOwner) {}
 
-    function safeMint(address to, uint256 score) public onlyOwner {
+    function safeMint(address to, uint256 score, string memory species) public onlyOwner {
         require(!_hasMinted[to], "This address has already minted an NFT.");
         require(score > _scoreThreshold, "Score must be greater than 6500.");
 
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _hasMinted[to] = true;
+
+        // Store the species of the token
+        _tokenSpecies[tokenId] = species;
+    }
+
+    // Get the species of a token
+    function getTokenSpecies(uint256 tokenId) public view returns (string memory) {
+        return _tokenSpecies[tokenId];
     }
 }
